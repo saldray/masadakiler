f03ad6b23d4faf5d34b8a9e2fb889e3488a749b2
saldray <othernother@yandex.com>
2023-12-14 23:48:27 +0300

    degiskenler

---
+++ java/degisken2.java
@@ -0,0 +1,18 @@
+public class degisken2 {
+    public static void main(String[] args) {
+        int x = 10;
+        int y;
+        int sadi;
+        sadi = 10;
+        System.out.println("Merhaba Dunya"+ sadi);
+        sadi = 20;
+        System.out.println("Merhaba Dunya"+ sadi);
+        //       fload pi = 3.14;//hata verir degisken degisimi yapılmalı
+        //        float pi = (float) 3.14;//1 yöntem
+        double pi = 3.14;
+        String s = "benim ismim sadi evren seker";
+        System.out.println(" dizgi " + s);
+        System.out.println(" pi : "+ pi);
+        System.out.println(" dizgi " + s +" pi :"+ pi );
+    }
+}
