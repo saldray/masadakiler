08c7d971bc058c3b1910c12dc6af9729475887e5
saldray <othernother@yandex.com>
2023-12-14 22:11:31 +0300

    ilk yorum listler.py eklendi

---
+++ listeler.py
@@ -0,0 +1,34 @@
+# l = [1, 2, 3, 4]
+# print(l)
+# l.append(55)
+# print(l)
+# l.insert(2, 111)
+# print(l)
+# l.remove(111)
+# print(l)
+
+l=[1, 2, 3, 4]
+l.append(55)
+print(l)
+l.insert(2, 111)
+l.append(111)
+print(l)
+l.remove(111)
+print(l)
+# ilk bulduğu 111'i siliyor.
+
+print(l.pop())
+print(l)
+
+print(l.index(2))
+print(l.index(55))
+
+#print(l.count(1))
+l.append(1)
+print(l.count(1))
+
+l.sort()
+print(l)
+
+# 8.49
+# https://www.youtube.com/watch?v=V7yZ69pgJKU&list=PLh9ECzBB8tJOoFYmIIiwFjgXDCD9uiD_i&index=1
