77d62e1bb47261f15e0f523607db2aa294d85dea
saldray <othernother@yandex.com>
2025-11-10 20:21:40 +0300

    zip ler

---
+++ kurs_malzemesiParcasi/github/small-program-0-first-last.c
@@ -0,0 +1,27 @@
+//********************************************************
+// small-program-0.c
+// Author: FirstName LastName
+// Date: 9/27/23
+// Class: COP 3223H, Professor Moran
+// Purpose: This program prints a specific message to the
+// command line. The main purpose of this assignment is to
+// check and see whether I can connect to the Eustis Server.
+// Input: None
+//
+// Output: (to the command line) A breif Message crafted by 
+// Dr. Moran to test running a C program on Eustis
+// //********************************************************
+
+
+// Header file for input output functions
+#include <stdio.h>
+ 
+// main function - where we will print the message
+int main()
+{
+ 
+    // prints the message to the command line.
+    printf("The current month is August.\nThe current year is 2023.\nI am a student at UCF.\nThis Fall 2023 I am taking Intro to Programming in C with Dr. Moran.\nI am super exicted to learn the C langauge this semester!!\n");
+ 
+    return 0;
+}
